4e9f86f9298e15b76d39573c4aaac3cfcc9337a7
prateekvarma <prtk.varma@gmail.com>
2023-05-01 15:04:00 -0600

    Structural html changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import MenuItem from "./MenuItem";
-import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
+import MenuItem from './MenuItem';
+import { AiFillHome, AiFillInfoCircle } from 'react-icons/ai';
 
 export default function Header() {
   return (
     <div>
-      <div className="flex">
+      <div className='flex'>
         <MenuItem title='HOME' address='/' Icon={AiFillHome} />
         <MenuItem title='ABOUT' address='/about' Icon={AiFillInfoCircle} />
       </div>
