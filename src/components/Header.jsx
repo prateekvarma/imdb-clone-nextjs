1afd027fe025134c4cf7ca5d1f7a64e35ebbc669
prateekvarma <prtk.varma@gmail.com>
2023-05-01 15:26:12 -0600

    Created and styled the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
+import Link from 'next/link';
 import MenuItem from './MenuItem';
 import { AiFillHome, AiFillInfoCircle } from 'react-icons/ai';
 
 export default function Header() {
   return (
-    <div>
+    <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
       <div className='flex'>
         <MenuItem title='HOME' address='/' Icon={AiFillHome} />
         <MenuItem title='ABOUT' address='/about' Icon={AiFillInfoCircle} />
       </div>
-      <div></div>
+      <div>
+        <Link href='/'>
+          <h2 className='text-2xl'>
+            <span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>
+              IMDb
+            </span>
+            <span className='text-xl hidden sm:inline'>Clone</span>
+          </h2>
+        </Link>
+      </div>
     </div>
   );
 }
