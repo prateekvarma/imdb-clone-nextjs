610777f6603d992740344d4980616ad379dd1998
prateekvarma <prtk.varma@gmail.com>
2023-05-02 13:33:51 -0600

    Installed next-themes package, and used it to create a Provider component

---
+++ src/app/Provider.jsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from "next-themes";
+
+function Provider() {
+    return ( <ThemeProvider>
+        
+    </ThemeProvider> );
+}
+
+export default Provider;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Provider from './Provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Provider>
+          <Header />
+          {children}
+        </Provider>
       </body>
     </html>
   );
