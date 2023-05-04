b4ce699301bd63e6faf0187281aff7d09305d985
prateekvarma <prtk.varma@gmail.com>
2023-05-04 16:29:38 -0600

    Used useEffect to load the theme button only after page hydrates

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -3,15 +3,20 @@
 import { MdLightMode } from 'react-icons/md';
 import { BsFillMoonFill } from 'react-icons/bs';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 function DarkModeSwitch() {
   const { systemTheme, theme, setTheme } = useTheme();
-
   const currentTheme = theme === 'system' ? systemTheme : theme;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true)
+  }, []);
 
   return (
     <>
-      {currentTheme === 'dark' ? (
+      {mounted && (currentTheme === 'dark' ? (
         <MdLightMode
           className='text-xl cursor-pointer hover:text-amber-500'
           onClick={() => setTheme('light')}
@@ -21,7 +26,7 @@ function DarkModeSwitch() {
           className='text-xl cursor-pointer hover:text-amber-500'
           onClick={() => setTheme('dark')}
         />
-      )}
+      ))}
     </>
   );
 }
