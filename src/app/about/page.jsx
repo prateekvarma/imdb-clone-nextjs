52a7910bfe30e4087d84fad2819a0a866f1c2f4f
prateekvarma <prtk.varma@gmail.com>
2023-05-05 14:37:14 -0600

    Styled and added content to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,56 @@
 function About() {
-    return ( <div>About page</div> );
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Augue mauris augue
+        neque gravida in fermentum. Fames ac turpis egestas sed tempus urna et.
+        Odio morbi quis commodo odio aenean sed adipiscing diam. Platea dictumst
+        quisque sagittis purus sit amet volutpat. Augue mauris augue neque
+        gravida in. Ut placerat orci nulla pellentesque dignissim enim. Lorem
+        donec massa sapien faucibus. Mauris a diam maecenas sed enim ut sem
+        viverra aliquet. Pharetra sit amet aliquam id diam maecenas. Amet
+        consectetur adipiscing elit pellentesque habitant morbi tristique
+        senectus et. Etiam tempor orci eu lobortis elementum nibh tellus
+        molestie nunc. Eget gravida cum sociis natoque penatibus et magnis.
+        Faucibus et molestie ac feugiat sed. Aliquam etiam erat velit
+        scelerisque. Tristique magna sit amet purus gravida quis blandit turpis.
+        Blandit libero volutpat sed cras ornare arcu dui vivamus arcu. Mauris
+        commodo quis imperdiet massa tincidunt nunc pulvinar sapien et.
+      </p>
+      <p>
+        Malesuada proin libero nunc consequat interdum. Amet justo donec enim
+        diam vulputate ut. At lectus urna duis convallis convallis tellus id.
+        Auctor eu augue ut lectus arcu bibendum. Vitae ultricies leo integer
+        malesuada nunc. Ullamcorper sit amet risus nullam. Aliquet nec
+        ullamcorper sit amet risus. Eu facilisis sed odio morbi quis. Ac turpis
+        egestas integer eget aliquet nibh praesent tristique magna. Cras
+        fermentum odio eu feugiat. Tellus pellentesque eu tincidunt tortor
+        aliquam nulla facilisi cras fermentum. Elit pellentesque habitant morbi
+        tristique senectus et. Integer malesuada nunc vel risus. Massa tincidunt
+        dui ut ornare lectus sit amet. Ac tortor dignissim convallis aenean et
+        tortor at risus viverra. Quisque id diam vel quam elementum pulvinar
+        etiam non. Venenatis cras sed felis eget velit aliquet. A iaculis at
+        erat pellentesque adipiscing commodo.
+      </p>
+      <p>
+        Malesuada proin libero nunc consequat interdum. Amet justo donec enim
+        diam vulputate ut. At lectus urna duis convallis convallis tellus id.
+        Auctor eu augue ut lectus arcu bibendum. Vitae ultricies leo integer
+        malesuada nunc. Ullamcorper sit amet risus nullam. Aliquet nec
+        ullamcorper sit amet risus. Eu facilisis sed odio morbi quis. Ac turpis
+        egestas integer eget aliquet nibh praesent tristique magna. Cras
+        fermentum odio eu feugiat. Tellus pellentesque eu tincidunt tortor
+        aliquam nulla facilisi cras fermentum. Elit pellentesque habitant morbi
+        tristique senectus et. Integer malesuada nunc vel risus. Massa tincidunt
+        dui ut ornare lectus sit amet. Ac tortor dignissim convallis aenean et
+        tortor at risus viverra. Quisque id diam vel quam elementum pulvinar
+        etiam non. Venenatis cras sed felis eget velit aliquet. A iaculis at
+        erat pellentesque adipiscing commodo.
+      </p>
+    </div>
+  );
 }
 
 export default About;
