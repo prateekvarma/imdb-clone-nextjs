34ee704da313208cda29655f1d1b335628f715b6
prateekvarma <prtk.varma@gmail.com>
2023-05-12 15:15:01 -0600

    Added styling to nav bar and elements

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function NavbarItem({ title, param }) {
   return (
     <div>
-      <Link href={`/?genre=${param}`}>{title}</Link>
+      <Link className={`m-4 hover:text-amber-600 font-semibold p-2`} href={`/?genre=${param}`}>{title}</Link>
     </div>
   );
 }
