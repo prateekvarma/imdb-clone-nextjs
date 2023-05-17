00cec99afe79107ef3cb1a584129c89f7b57867a
prateekvarma <prtk.varma@gmail.com>
2023-05-17 15:07:00 -0600

    Fetching tmdb API data from home server component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
-import Image from 'next/image';
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(`https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}`)
   return <h1>Home</h1>;
 }
