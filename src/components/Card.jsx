8958e6319bf5b4d4bf3663d126ccd7e94752bbcb
prateekvarma <prtk.varma@gmail.com>
2023-05-24 16:29:03 -0600

    Displaying image in card, and hover effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 function Card({ result }) {
-  return <div>{result.id}</div>;
+  return (
+    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
+        ></Image>
+      </Link>
+    </div>
+  );
 }
 
 export default Card;
