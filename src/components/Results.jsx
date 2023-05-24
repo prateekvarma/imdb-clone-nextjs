57c36d449a8a8560d95701c4c3b35212033a404a
prateekvarma <prtk.varma@gmail.com>
2023-05-24 16:10:08 -0600

    Added responsive grid

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 function Results({ results }) {
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results.map((result) => {
         return <Card key={result.id} result={result} />;
       })}
