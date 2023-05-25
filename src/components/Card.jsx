7599574aed204f819773989835afca3e9b8d43a2
prateekvarma <prtk.varma@gmail.com>
2023-05-25 17:13:52 -0600

    Finished card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { FiThumbsUp } from 'react-icons/fi';
+
 function Card({ result }) {
   return (
     <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
@@ -14,7 +16,19 @@ function Card({ result }) {
           className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
           placeholder='blur'
           blurDataURL='/public/Bean-Eater-spinner.svg'
+          alt='movie poster image'
+          style={{ maxWidth: '100%', height: 'auto' }}
         ></Image>
+        <div className='p-2'>
+          <p className='line-clamp-2 text-md'>{result.overview}</p>
+          <h2 className='truncate text-lg font-bold'>
+            {result.title || result.name}
+          </h2>
+          <p className='flex items-center'>
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className='h-5 mr-1 ml-3' /> {result.vote_count}
+          </p>
+        </div>
       </Link>
     </div>
   );
